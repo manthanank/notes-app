9cbe8afabc77dd331cc5e7e6cf4e1c05d1993dcd
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-07-30 23:21:54 +0530

    chore: Update environment file to use correct API URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl:
+  production: false,
+  apiUrl:
     window.location.hostname === 'localhost'
       ? 'http://localhost:3000/api'
       : 'https://notes-app-8y8i.vercel.app/api',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',
+  production: false,
+  apiUrl:
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:3000/api'
+      : 'https://notes-app-eta-coral.vercel.app/api',
 };
