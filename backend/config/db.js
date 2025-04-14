a988d56ac13b7debfa33256736cb07310cb1bd88
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-04-14 09:46:40 +0530

    fix: update license year and refine MongoDB connection configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,11 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const dbUser = process.env.MONGODB_USER;
-const dbPassword = process.env.MONGODB_PASSWORD;
-const dbName = process.env.MONGODB_DBNAME || 'notesdb';
-
-const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.re3ha3x.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const mongoURI = process.env.MONGODB_URI;
 
 module.exports = async function connectDB() {
     try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true, useUnifiedTopology: true
-        });
+        await mongoose.connect(mongoURI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed');
